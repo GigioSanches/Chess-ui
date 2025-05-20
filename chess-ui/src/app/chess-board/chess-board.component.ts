f4aa62a1b48aac2f8f8b3a216768774c17e3971e
GigioSanches <giovanni.sanches27@gmail.com>
2025-05-20 11:22:14 -0300

    r Refatorado layout do tabuleiro, implementado logica de comunicação com backend em java, tudo pronto para iniciar a implementação de movimento

--- chess-ui/src/app/chess-board/chess-board.component.ts
+++ chess-ui/src/app/chess-board/chess-board.component.ts
@@ -4,22 +4,24 @@ import { CommonModule } from '@angular/common';
 
 interface Cell {
   name: string | null | undefined;
+  color: string | null | undefined; 
+  x: number;
+  y: number;
 }
-
 @Component({
   selector: 'app-chess-board',
   standalone: true,
   imports: [CommonModule, HttpClientModule], 
   templateUrl: './chess-board.component.html',
-  styleUrl: './chess-board.component.css'
+  styleUrls: ['./chess-board.component.css']
 })
 export class ChessBoardComponent {
-  board: Array<Array<Cell>> = []; // Definindo o tipo correto
+  board: Array<Array<Cell>> = []; 
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    console.log('ngOnInit chamado'); // Verifica o conteúdo de 'board'
+    console.log('ngOnInit chamado'); 
     this.fetchChessBoard();
   }
 
@@ -27,6 +29,22 @@ export class ChessBoardComponent {
     this.http.get<Array<Array<Cell>>>('/api/chessboard').subscribe(data => {
       this.board = data;
       console.log(this.board);
+    });
+  }
+
+  ExibePosicao(cell: any): void {
+
+    const piece = {
+      name: cell.name,
+      color: cell.color,
+      x: cell.x,
+      y: cell.y
+    }
+
+
+    this.http.post('/api/ExibePosicao', piece).subscribe({
+      next: () => console.log('Objeto enviado ao backend:', piece),
+      error: (err) => console.error('Erro ao enviar objeto:', err)
     });
   }
 }
